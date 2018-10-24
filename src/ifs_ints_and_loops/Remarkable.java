9bdb29152a21e13b10613e0945b049ad51db3e9e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-24 13:55:44 -0700

    Unbirthday and Remarkable, IP

---
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -0,0 +1,18 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+public static void main(String[] args) {
+	String Angel="Coolest guy ever";
+	String Omar= "Finished skyrim at level 4";
+	String Mohammed="Watches Dub trash";
+	String Lam="Looks like an anime character";
+	String Gabriel="Never talks to me";
+	String Khadar="Never does his work";
+	String Timmy="Overwatch extremist";
+	String Eric="Lost his glasses in the 7th grade and we almost stepped on them at the bridge";
+	String name=JOptionPane.showInputDialog("What's your name?");
+	
+}
+}
---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,15 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	String birthday=JOptionPane.showInputDialog("When is your birthday?");
+	if(birthday.equals("October 24"))
+		JOptionPane.showMessageDialog(null, "Happy Birhday");
+}
+	else {
+		JOptionPane.showMessageDialog(null,"Very Merry Birthday!");
+	}
+}
+}
