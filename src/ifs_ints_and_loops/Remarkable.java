dc69f04243529de21473dcb8eef5f11a9c884307
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-31 13:15:31 -0700

    Remarkable

--- src/ifs_ints_and_loops/Remarkable.java
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -6,13 +6,40 @@ public class Remarkable {
 public static void main(String[] args) {
 	String Angel="Coolest guy ever";
 	String Omar= "Finished skyrim at level 4";
-	String Mohammed="Watches Dub trash";
+	String Mohammed="Watches Dub";
 	String Lam="Looks like an anime character";
 	String Gabriel="Never talks to me";
-	String Khadar="Never does his work";
-	String Timmy="Overwatch extremist";
+	String Khadar="Does stuff";
+	String Timmy="Plays pc";
 	String Eric="Lost his glasses in the 7th grade and we almost stepped on them at the bridge";
 	String name=JOptionPane.showInputDialog("What's your name?");
 	
+	if(name.equals(Angel)) {
+		JOptionPane.showMessageDialog(null, Angel);
+	}
+	if(name.equals(Omar)) {
+		JOptionPane.showMessageDialog(null, Omar);
+	}
+	if(name.equals(Mohammed)) {
+		JOptionPane.showMessageDialog(null, Mohammed);
+	}
+	if(name.equals(Lam)) {
+		JOptionPane.showMessageDialog(null, Lam);
+	}
+	if(name.equals(Gabriel)) {
+		JOptionPane.showMessageDialog(null, Gabriel);
+	}
+	if(name.equals(Khadar)) {
+		JOptionPane.showMessageDialog(null, Khadar);
+	}
+	if(name.equals(Timmy)) {
+		JOptionPane.showMessageDialog(null, Timmy);
+	}
+	if(name.equals(Eric)) {
+		JOptionPane.showMessageDialog(null, Eric);
+	}
+
+
 }
 }
+
--- src/ifs_ints_and_loops/Unbirthday.java
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -6,7 +6,7 @@ public class Unbirthday {
 public static void main(String[] args) {
 	String birthday=JOptionPane.showInputDialog("When is your birthday?");
 	if(birthday.equals("October 24"))
-		JOptionPane.showMessageDialog(null, "Happy Birhday");
+		JOptionPane.showMessageDialog(null, "Happy Birhday!");
 }
 	else {
 		JOptionPane.showMessageDialog(null,"Very Merry Birthday!");
